602467d9b04147d0579dc9b0b5f74c95b562146b
yejeungJi <jiyejeung@gmail.com>
2022-06-15 17:46:46 +0900

    Learning Vue Function and Event

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-	<div class="divNameContainer">{{ name }}</div>
+	<div class="divNameContainer">{{ greeting(name) }} {{ age }}</div>
+	<button class="buttonExample" v-on:click="clickExample()">CLICK</button>
 </template>
 <script>
 export default {
 	setup() {
 		const name = 'Yejeung';
-		return { name };
+		const age = 27;
+		const greeting = nameArg => 'Hello ' + nameArg;
+
+		const clickExample = () => void console.log(greeting(name));
+		return { name, age, greeting, clickExample };
 	},
 };
 </script>
