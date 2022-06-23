a107f10233b7721d78a3d792cfcb7d5c393b5f89
yejeungJi <jiyejeung@gmail.com>
2022-06-23 21:19:51 +0900

    AddToDo.vue 파일 수정 (ADD 버튼을 클릭하면, input의 내용을 토대로 totalToDos배열내의 값을 추가하고, json 가상 데이터공간에도 값을 추가하는 기능 추가)

--- src/components/AddToDo.vue
+++ src/components/AddToDo.vue
@@ -1,13 +1,41 @@
 <template>
-	
+	<form class="formAddToDoContainer" @submit.prevent="submitToAdd">
+		<input class="inputAddToDo" type="text" placeholder="Add..." v-model="toDoKeyword" />
+		<button class="buttonAddToDo" type="submit">ADD</button>
+	</form>
 </template>
 
 <script>
-export default {
+import { ref } from 'vue';
 
-}
+export default {
+	setup(_, context) {
+		const toDoKeyword = ref('');
+		const submitToAdd = () => {
+			context.emit('add-keyword', toDoKeyword.value);
+		};
+		return { toDoKeyword, submitToAdd };
+	},
+};
 </script>
 
 <style>
-
+.formAddToDoContainer {
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-bottom: 3vw;
+}
+.inputAddToDo {
+	width: 40%;
+	font-size: 2vw;
+	margin-right: 1vw;
+	padding-left: 1vw;
+}
+.buttonAddToDo {
+	width: 15%;
+	font-size: 2vw;
+	cursor: pointer;
+}
 </style>
