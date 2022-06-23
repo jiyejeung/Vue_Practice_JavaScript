a9751b44e920640a0c9e60b5ffa103c99c401af2
yejeungJi <jiyejeung@gmail.com>
2022-06-23 21:22:20 +0900

    SearchToDo.vue 파일 수정 (Search 버튼을 클릭하면, 배열 내에 input의 값이 존재하는지 확인 후, 화면에 출력하는 기능추가)

--- src/components/SearchToDo.vue
+++ src/components/SearchToDo.vue
@@ -1,13 +1,44 @@
 <template>
-	
+	<form class="formSearchToDoContainer" @submit.prevent="submitToSearch">
+		<input class="inputSearchToDo" type="text" placeholder="Search..." v-model="searchKeyword" @keydown="submitToSearch" />
+		<button class="buttonSearchToDo" type="submit">SEARCH</button>
+	</form>
 </template>
 
 <script>
+import { ref, watch } from 'vue';
 export default {
-
-}
+	emits: ['search-keyword'],
+	setup(_, context) {
+		const searchKeyword = ref('');
+		watch(searchKeyword, () => {
+			submitToSearch();
+		});
+		const submitToSearch = () => {
+			context.emit('search-keyword', searchKeyword.value);
+		};
+		return { searchKeyword, submitToSearch };
+	},
+};
 </script>
 
 <style>
-
+.formSearchToDoContainer {
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-bottom: 3vw;
+}
+.inputSearchToDo {
+	width: 40%;
+	font-size: 2vw;
+	margin-right: 1vw;
+	padding-left: 1vw;
+}
+.buttonSearchToDo {
+	width: 15%;
+	font-size: 2vw;
+	cursor: pointer;
+}
 </style>
